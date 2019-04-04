fa68d33b63f6a0f4924aa3ba1c6e5729102306f8
Draghi96 <48287401+Draghi96@users.noreply.github.com>
2019-04-04 23:23:50 +0200

    Added "EmptyDeckException" Exception class file

---
+++ src/main/java/it/polimi/ingsw/EmptyDeckException.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw;
+
+/**
+ * This class represents all exceptions related to a deck being empty.
+ */
+public class EmptyDeckException extends Exception{
+
+    /**
+     * This method is used to create a new exception that is related to a deck being empty.
+     */
+    public EmptyDeckException() {
+    }
+
+    /**
+     * This method is used to create a new exception that is related to a deck being empty that also
+     * generates a message.
+     */
+    public EmptyDeckException(String message) {
+        super(message);
+    }
+}
