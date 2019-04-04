ec9c6eb467179059384d1b53443400d4322926aa
Draghi96 <48287401+Draghi96@users.noreply.github.com>
2019-04-04 23:18:47 +0200

    Added "Card" interface file

---
+++ src/main/java/it/polimi/ingsw/Card.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+/**
+ * This class represents one generic single card.
+ * <p>A card can be a Weapon card, a Powerup card or an AmmoTile card.</p>
+ *
+ * @author Draghi96
+ * @see Weapon
+ * @see Powerup
+ * @see AmmoTile
+ */
+public interface Card {
+}
