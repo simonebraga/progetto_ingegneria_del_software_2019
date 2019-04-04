2a695eaa9507f88fe292f3cbe04d90ed885acf30
Draghi96 <48287401+Draghi96@users.noreply.github.com>
2019-04-04 20:39:16 +0200

    Added "PowerupName" enumeration file

---
+++ src/main/java/it/polimi/ingsw/PowerupName.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+/**
+ * This enumeration will be used to distinguish all powerups.
+ *
+ * @author Draghi96
+ */
+public enum PowerupName {
+    TARGETINGSCOPE,
+    NEWTON,
+    TAGBACKGRENADE,
+    TELEPORTER
+}
