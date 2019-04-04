92270f60827957549d6f40a5aa20a5da154a7bfd
Draghi96 <48287401+Draghi96@users.noreply.github.com>
2019-04-04 20:39:59 +0200

    Added "WeaponName" enumeration file

---
+++ src/main/java/it/polimi/ingsw/WeaponName.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw;
+
+/**
+ * This enumeration will be use to distinguish all weapons.
+ *
+ * @author Draghi96
+ */
+public enum WeaponName {
+    LOCKRIFLE,
+    MACHINEGUN,
+    THOR,
+    PLASMAGUN,
+    WHISPER,
+    ELECTROSCYTHE,
+    TRACTORBEAM,
+    VORTEXCANNON,
+    FURNACE,
+    HEATSEEKER,
+    HELLION,
+    FLAMETHROWER,
+    GRENADELAUNCHER,
+    ROCKETLAUNCHER,
+    RAILGUN,
+    CYBERBLADE,
+    ZX2,
+    SHOTGUN,
+    POWERGLOVE,
+    SHOCKWAVE,
+    SLEDGEHAMMER
+}
