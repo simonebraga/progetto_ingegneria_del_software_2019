5f178b8f33dec13be3547cdaa388d79ff768379b
Draghi96 <48287401+Draghi96@users.noreply.github.com>
2019-04-05 20:23:41 +0200

    Added "Effect" interface file

---
+++ src/main/java/it/polimi/ingsw/Effect.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw;
+
+/**
+ * This interface is implemented by all possible effects of the game.
+ *
+ * @author Draghi96
+ */
+public interface Effect {
+
+    /**
+     * This method will be implemented by each effect to cause its related effect.
+     */
+    void doAction();
+}
