b45f549653c17a0bf87e7cad9f0c0f58c7657dcb
Draghi96 <48287401+Draghi96@users.noreply.github.com>
2019-04-05 15:59:07 +0200

    Updated "TestDeck" test class file

--- src/test/java/it/polimi/ingsw/TestDeck.java
+++ src/test/java/it/polimi/ingsw/TestDeck.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -14,48 +16,76 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestDeck {
 
     /**
+     * This attribute is the list of cards that can be drawn that composes half of a deck.
+     */
+    private ArrayList<Card> activeList;
+
+    /**
+     * This attribute is the list of cards that cannot be drawn that composes half of a deck.
+     */
+    private ArrayList<Card> inactiveList;
+
+    /**
+     * This is a deck used for all tests.
+     */
+    private Deck deck1;
+
+    /**
+     * This method sets up all required object to test all methods in this suit.
+     */
+    @BeforeEach
+    void setUp(){
+
+        //creating a list for activeCards
+        ArrayList<Color> price = new ArrayList<>();
+        price.clear();
+        activeList = new ArrayList<>();
+        activeList.add(new Weapon(price,WeaponName.LOCKRIFLE,true));
+        activeList.add(new Weapon(price,WeaponName.MACHINEGUN,true));
+        activeList.add(new Weapon(price,WeaponName.THOR,true));
+        activeList.add(new Weapon(price,WeaponName.PLASMAGUN,true));
+        activeList.add(new Weapon(price,WeaponName.WHISPER,true));
+        activeList.add(new Weapon(price,WeaponName.ELECTROSCYTHE,true));
+        activeList.add(new Weapon(price,WeaponName.TRACTORBEAM,true));
+        activeList.add(new Weapon(price,WeaponName.VORTEXCANNON,true));
+        activeList.add(new Weapon(price,WeaponName.FURNACE,true));
+        activeList.add(new Weapon(price,WeaponName.HEATSEEKER,true));
+        activeList.add(new Weapon(price,WeaponName.HELLION,true));
+        activeList.add(new Weapon(price,WeaponName.FLAMETHROWER,true));
+        activeList.add(new Weapon(price,WeaponName.GRENADELAUNCHER,true));
+        activeList.add(new Weapon(price,WeaponName.ROCKETLAUNCHER,true));
+        activeList.add(new Weapon(price,WeaponName.RAILGUN,true));
+        activeList.add(new Weapon(price,WeaponName.CYBERBLADE,true));
+        activeList.add(new Weapon(price,WeaponName.ZX2,true));
+        activeList.add(new Weapon(price,WeaponName.SHOTGUN,true));
+        activeList.add(new Weapon(price,WeaponName.POWERGLOVE,true));
+        activeList.add(new Weapon(price,WeaponName.SHOCKWAVE,true));
+        activeList.add(new Weapon(price,WeaponName.SLEDGEHAMMER,true));
+
+        //creating a list for inactiveCards
+        inactiveList = new ArrayList<>();
+        inactiveList.add(new Powerup(Color.RED,PowerupName.TELEPORTER));
+        inactiveList.add(new Powerup(Color.YELLOW,PowerupName.TELEPORTER));
+        inactiveList.add(new Powerup(Color.BLUE,PowerupName.TARGETINGSCOPE));
+        inactiveList.add(new Powerup(Color.RED,PowerupName.NEWTON));
+        inactiveList.add(new Powerup(Color.RED,PowerupName.TAGBACKGRENADE));
+
+        //creating deck
+        deck1= new Deck(activeList,inactiveList);
+    }
+
+    /**
      * This test verifies that shuffle() changes at least one card position inside the list.
      * <p>The list of cards cannot be empty.</p>
+     *
+     * @throws EmptyDeckException if the activeCards list is empty.
      */
     @Test
     void shuffleChangesOrder() throws EmptyDeckException {
-        //creating deck
-        ArrayList<Color> price = new ArrayList<>();
-        price.clear();
 
-        ArrayList<Card> inactiveList1 = new ArrayList<>();
-        inactiveList1.add(new Powerup(Color.RED,PowerupName.TELEPORTER));
-        inactiveList1.add(new Powerup(Color.YELLOW,PowerupName.TELEPORTER));
-        inactiveList1.add(new Powerup(Color.BLUE,PowerupName.TARGETINGSCOPE));
-        inactiveList1.add(new Powerup(Color.RED,PowerupName.NEWTON));
-        inactiveList1.add(new Powerup(Color.RED,PowerupName.TAGBACKGRENADE));
-        ArrayList<Card> inactiveList2 = new ArrayList<>(inactiveList1);
+        ArrayList<Card> inactiveList2 = new ArrayList<>(inactiveList);
+        ArrayList<Card> activeList2 = new ArrayList<>(activeList);
 
-        ArrayList<Card> activeList1 = new ArrayList<>();
-        activeList1.add(new Weapon(price,WeaponName.LOCKRIFLE,true));
-        activeList1.add(new Weapon(price,WeaponName.MACHINEGUN,true));
-        activeList1.add(new Weapon(price,WeaponName.THOR,true));
-        activeList1.add(new Weapon(price,WeaponName.PLASMAGUN,true));
-        activeList1.add(new Weapon(price,WeaponName.WHISPER,true));
-        activeList1.add(new Weapon(price,WeaponName.ELECTROSCYTHE,true));
-        activeList1.add(new Weapon(price,WeaponName.TRACTORBEAM,true));
-        activeList1.add(new Weapon(price,WeaponName.VORTEXCANNON,true));
-        activeList1.add(new Weapon(price,WeaponName.FURNACE,true));
-        activeList1.add(new Weapon(price,WeaponName.HEATSEEKER,true));
-        activeList1.add(new Weapon(price,WeaponName.HELLION,true));
-        activeList1.add(new Weapon(price,WeaponName.FLAMETHROWER,true));
-        activeList1.add(new Weapon(price,WeaponName.GRENADELAUNCHER,true));
-        activeList1.add(new Weapon(price,WeaponName.ROCKETLAUNCHER,true));
-        activeList1.add(new Weapon(price,WeaponName.RAILGUN,true));
-        activeList1.add(new Weapon(price,WeaponName.CYBERBLADE,true));
-        activeList1.add(new Weapon(price,WeaponName.ZX2,true));
-        activeList1.add(new Weapon(price,WeaponName.SHOTGUN,true));
-        activeList1.add(new Weapon(price,WeaponName.POWERGLOVE,true));
-        activeList1.add(new Weapon(price,WeaponName.SHOCKWAVE,true));
-        activeList1.add(new Weapon(price,WeaponName.SLEDGEHAMMER,true));
-        ArrayList<Card> activeList2 = new ArrayList<>(activeList1);
-
-        Deck deck1 = new Deck(activeList1,inactiveList1);
         Deck deck2 = new Deck(activeList2,inactiveList2);
 
         deck1.shuffle(); //method usage
@@ -67,79 +97,68 @@ class TestDeck {
 
     /**
      * This test verifies if reset() add two decks together.
-     * <p>First list must be empty.<br/>
-     * Second list cannot be empty.</p>
+     * <p>activeCards list must be empty.<br/>
+     * inactiveCards list cannot be empty.</p>
+     *
+     * @throws EmptyDeckException if the inactiveCards list is empty.
+     * @throws NotEmptyDeckException if the activeCards list is not empty.
      */
     @Test
     void resetAddsTwoDecks() throws EmptyDeckException, NotEmptyDeckException {
-        //creating 2 list of cards
-        ArrayList<Card> inactivePowerups = new ArrayList<>();
-        ArrayList<Card> activePowerups = new ArrayList<>();
-        activePowerups.clear();
-        inactivePowerups.add(new Powerup(Color.RED, PowerupName.NEWTON));
-        inactivePowerups.add(new Powerup(Color.YELLOW, PowerupName.NEWTON));
-        inactivePowerups.add(new Powerup(Color.BLUE,PowerupName.NEWTON));
 
-        //creating deck
-        Deck deck = new Deck(activePowerups,inactivePowerups);
+        int oldDimActive = deck1.getActiveCards().size();
+        int oldDimInactive = deck1.getInactiveCards().size();
 
-        int oldDimActive = deck.getActiveCards().size();
-        int oldDimInactive = deck.getInactiveCards().size();
+        deck1.reset();   //method usage
 
-        deck.reset();   //method usage
-
-        assertEquals(deck.getActiveCards().size(),oldDimInactive);
-        assertTrue(deck.getInactiveCards().isEmpty());
-        assertEquals(deck.getActiveCards().size(),oldDimActive+oldDimInactive);
+        assertEquals(oldDimInactive,deck1.getActiveCards().size());
+        assertTrue(deck1.getInactiveCards().isEmpty());
+        assertEquals(oldDimActive + oldDimInactive,deck1.getActiveCards().size());
     }
 
     /**
      * This test verifies if draw() returns a card from deck.
+     * <p>If the activeCards list is empty it automatically calls reset() and then tries again.</p>
+     *
+     * @throws EmptyDeckException if the activeCards list is empty.
+     * @throws NotEmptyDeckException if the inactiveCards list is empty.
      */
     @Test
     void drawReturnsCardAndDeckSizeIsDecreasing() throws EmptyDeckException, NotEmptyDeckException {
-        //creating 5-powerup deck
-        ArrayList<Card> activePowerups = new ArrayList<>();
-        ArrayList<Card> inactivePowerups = new ArrayList<>();
-        inactivePowerups.clear();
-        activePowerups.add(new Powerup(Color.RED, PowerupName.NEWTON));
-        activePowerups.add(new Powerup(Color.BLUE, PowerupName.NEWTON));
-        activePowerups.add(new Powerup(Color.YELLOW, PowerupName.NEWTON));
-        activePowerups.add(new Powerup(Color.YELLOW, PowerupName.TAGBACKGRENADE));
-        activePowerups.add(new Powerup(Color.YELLOW, PowerupName.TELEPORTER));
 
-        Deck deck = new Deck(activePowerups,inactivePowerups);
+        int oldActiveDim=activeList.size();
+        int oldInactiveDim=inactiveList.size();
 
-        int oldActiveDim=activePowerups.size();
-        int oldInactiveDim=inactivePowerups.size();
-
-        Card output = deck.draw();  //method usage
+        Card output = deck1.draw();  //method usage
 
         assertNotNull(output);
-        assertEquals(oldActiveDim-1,deck.getActiveCards().size());
-        assertEquals(oldInactiveDim,deck.getInactiveCards().size());
+        assertEquals(oldActiveDim-1,deck1.getActiveCards().size());
+        assertEquals(oldInactiveDim,deck1.getInactiveCards().size());
     }
 
     /**
-     *This test verifies if discard() adds a card passed as parameter into the inactiveCards list of a deck.
+     *This test verifies if discard() adds a card passed as parameter into the inactiveCards
+     *  list of a deck.
      */
     @Test
     void discardAddsCardToInactiveList() {
-        //creating lists
-        ArrayList<Card> activePowerups = new ArrayList<>();
-        ArrayList<Card> inactivePowerups = new ArrayList<>();
 
-        activePowerups.add(new Powerup(Color.RED,PowerupName.NEWTON));
-        inactivePowerups.add(new Powerup(Color.BLUE,PowerupName.TELEPORTER));
+        int oldActiveDim = activeList.size();
+        int oldInactiveDim = inactiveList.size();
 
-        Deck deck = new Deck(activePowerups,inactivePowerups);
+        deck1.discard(new Powerup(Color.YELLOW,PowerupName.TARGETINGSCOPE)); //method usage
 
-        int oldActiveDim = activePowerups.size();
-        int oldInactiveDim = inactivePowerups.size();
+        assertEquals(oldActiveDim, deck1.getActiveCards().size());
+        assertEquals(oldInactiveDim + 1,deck1.getInactiveCards().size());
+    }
 
-        deck.discard(new Powerup(Color.YELLOW,PowerupName.TARGETINGSCOPE)); //method usage
-
-        assertEquals(oldActiveDim, deck.getActiveCards().size());
-        assertEquals(oldInactiveDim+1,deck.getInactiveCards().size());
+    /**
+     * This method frees all object created for this test suit.
+     */
+    @AfterAll
+    void tearDown(){
+        activeList=null;
+        inactiveList=null;
+        deck1=null;
     }
 }
