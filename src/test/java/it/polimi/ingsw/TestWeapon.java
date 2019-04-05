eee8d91b4093f4cf8286ca05b0145cce5fb7342f
Draghi96 <48287401+Draghi96@users.noreply.github.com>
2019-04-05 16:09:05 +0200

    Updated "TestWeapon" test class file

--- src/test/java/it/polimi/ingsw/TestWeapon.java
+++ src/test/java/it/polimi/ingsw/TestWeapon.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -14,18 +16,44 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestWeapon {
 
     /**
-     * This test verifies that the reload() operation actually set the isLoaded attribute to true.
+     * This attribute is a list of Color that represent a price for a weapon that will be used for testing.
+     */
+    ArrayList<Color> price;
+
+    /**
+     * This attribute is a Weapon object that will be used for testing.
+     */
+    Weapon shotgun;
+
+    /**
+     * This method creates all objects used to test all methods in this test suit.
+     */
+    @BeforeEach
+    void setUp(){
+        //creating unloaded shotgun
+        price = new ArrayList<>();
+        price.add(Color.YELLOW);
+        price.add(Color.RED);
+        shotgun = new Weapon(price,WeaponName.SHOTGUN,false);
+    }
+
+    /**
+     * This test verifies that the reload() operation set the isLoaded attribute to true.
      */
     @Test
     void reloadSetsIsLoadedToTrue() throws WeaponAlreadyLoadedException {
-        //creating unloaded shotgun
-        ArrayList<Color> price = new ArrayList<>();
-        price.add(Color.YELLOW);
-        price.add(Color.YELLOW);
-        Weapon shotgun = new Weapon(price,WeaponName.SHOTGUN,false);
 
-        shotgun.reload(); //should reload it
+        shotgun.reload(); //method usage
 
         assertTrue(shotgun.getLoaded());
+    }
+
+    /**
+     * This method frees all objects used to test all methods in this test suit.
+     */
+    @AfterAll
+    void tearDown(){
+        price=null;
+        shotgun=null;
     }
 }
