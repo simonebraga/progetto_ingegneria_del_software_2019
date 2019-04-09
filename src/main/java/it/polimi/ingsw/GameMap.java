c03cc9daca6f8806582d3545eff599863525bb89
AleCarminati <48724629+AleCarminati@users.noreply.github.com>
2019-04-09 12:27:00 +0200

    Added class GameMap and the tests for this class.

---
+++ src/main/java/it/polimi/ingsw/GameMap.java
@@ -0,0 +1,220 @@
+package it.polimi.ingsw;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * Represents the map of the game.
+ */
+public class GameMap {
+
+    /**
+     * Contains the Squares of the map and the position of each Square in the map.
+     * <p>It can contain some Square null when there is no Square in that position</p>
+     */
+    private final Square[][] grid;
+
+    /**
+     * Contains the list of Squares that are SpawnSquare. It makes faster to see which Squares
+     * are SpawnSquare.
+     */
+    private final ArrayList<Square> spawnSquares;
+
+    /**
+     * Contains the list of Squares that are TileSquare. It makes faster to see which Squares
+     * are TileSquare.
+     */
+    private final ArrayList<Square> tileSquares;
+
+    public GameMap(Square[][] grid, ArrayList<Square> spawnSquares, ArrayList<Square> tileSquares) {
+        this.grid = grid;
+        this.spawnSquares = spawnSquares;
+        this.tileSquares = tileSquares;
+    }
+
+    public Square[][] getGrid() {
+        return grid;
+    }
+
+    public ArrayList<Square> getSpawnSquares() {
+        return spawnSquares;
+    }
+
+    public ArrayList<Square> getTileSquares() {
+        return tileSquares;
+    }
+
+    /**
+     * Return the coordinates of the Square passed by parameter.
+     * @param square The Square of which the method will find the coordinates.
+     * @return A two-Integer Array, the first value is the x coordinate, the second value is
+     * the y coordinate.
+     */
+    public ArrayList<Integer> getCoord(Square square){
+        int i;
+        int j;
+        ArrayList<Integer> coordinates = new ArrayList<>(2);
+        for(i = 0; i<getGrid()[0].length; i++){
+            for(j = 0; j<getGrid().length; j++){
+                if(square == getGrid()[i][j]){
+                    coordinates.add(i);
+                    coordinates.add(j);
+                    return coordinates;
+                }
+            }
+        }
+        return coordinates;
+    }
+
+    /**
+     * Returns the Square in the coordinates (x, y).
+     * <p>It can return a null pointer if there is no Square in that coordinates.
+     * The caller of the method must control that the coordinates are not negative and that they
+     * are lower than the max coordinate possible</p>
+     * @param x The first coordinate of the Square.
+     * @param y The second coordinate of the Square.
+     * @return The Square in the coordinates (x, y).
+     */
+    public Square getSquare(Integer x, Integer y){
+        return getGrid()[x][y];
+    }
+
+    /**
+     * Returns all the Square that are in the same room of the Square passed by parameter.
+     * <p>The method returns also the Square passed by parameter.</p>
+     * @param square The Square of which the method will find the room.
+     * @return The List of the Squares that compose the room of the Square passed by parameter.
+     */
+    public ArrayList<Square> getRoom(Square square){
+        Square squareAnalyzing;
+        Square squareTemp;
+        Integer xTemp;
+        Integer yTemp;
+        ArrayList<Square> squaresToAnalyze = new ArrayList<>();
+        ArrayList<Square> squaresToReturn = new ArrayList<>();
+        squaresToAnalyze.add(square);
+        while(!squaresToAnalyze.isEmpty()){
+            squareAnalyzing = squaresToAnalyze.get(0);
+            xTemp = getCoord(squareAnalyzing).get(0);
+            yTemp = getCoord(squareAnalyzing).get(1);
+            if(squareAnalyzing.getUp() == Border.NOTHING){
+                squareTemp = getSquare(xTemp - 1, yTemp);
+                if(!squaresToAnalyze.contains(squareTemp) && !squaresToReturn.contains(squareTemp)){
+                    squaresToAnalyze.add(squareTemp);
+                }
+            }
+            if(squareAnalyzing.getDown() == Border.NOTHING){
+                squareTemp = getSquare(xTemp + 1, yTemp);
+                if(!squaresToAnalyze.contains(squareTemp) && !squaresToReturn.contains(squareTemp)){
+                    squaresToAnalyze.add(squareTemp);
+                }
+            }
+            if(squareAnalyzing.getLeft() == Border.NOTHING){
+                squareTemp = getSquare(xTemp, yTemp - 1);
+                if(!squaresToAnalyze.contains(squareTemp) && !squaresToReturn.contains(squareTemp)){
+                    squaresToAnalyze.add(squareTemp);
+                }
+            }
+            if(squareAnalyzing.getRight() == Border.NOTHING){
+                squareTemp = getSquare(xTemp, yTemp + 1);
+                if(!squaresToAnalyze.contains(squareTemp) && !squaresToReturn.contains(squareTemp)){
+                    squaresToAnalyze.add(squareTemp);
+                }
+            }
+            squaresToReturn.add(squareAnalyzing);
+            squaresToAnalyze.remove(0);
+        }
+        return squaresToReturn;
+    }
+
+    /**
+     * Return all the Squares that are visible from the Square passed by parameter.
+     * <p>The method returns also the Square passed by parameter</p>
+     * @param square The Square of which the method will find the visibility.
+     * @return The List of the Square that are visible from the Square passed by parameter.
+     */
+    public ArrayList<Square> getVisibility(Square square){
+        ArrayList<Square> squaresToReturn = new ArrayList<>();
+        ArrayList<Integer> coordinates = getCoord(square);
+        Integer xTemp = coordinates.get(0);
+        Integer yTemp = coordinates.get(1);
+        squaresToReturn.addAll(getRoom(square));
+        if(square.getUp() == Border.DOOR){
+            squaresToReturn.addAll(getRoom(getSquare(xTemp - 1, yTemp)));
+        }
+        if(square.getDown() == Border.DOOR){
+            squaresToReturn.addAll(getRoom(getSquare(xTemp + 1, yTemp)));
+        }
+        if(square.getLeft() == Border.DOOR){
+            squaresToReturn.addAll(getRoom(getSquare(xTemp, yTemp - 1)));
+        }
+        if(square.getRight() == Border.DOOR){
+            squaresToReturn.addAll(getRoom(getSquare(xTemp, yTemp + 1)));
+        }
+        return squaresToReturn.stream().distinct().collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    /**
+     * Returns all the Squares that can be reached with the number of moves passed by the parameter
+     * or less.
+     * <p>Attention! The Squares can be not visible from the starting square.</p>
+     * @param range The maximum number of moves that can be done.
+     * @param square The starting Square to calculate the range.
+     * @return The list of Squares that can be reached with the number of moves passed by
+     * the parameter or less. This list contains no repetitions.
+     */
+    public ArrayList<Square> getRange(Square square, Integer range){
+        ArrayList<Square> squaresToReturn = new ArrayList<>();
+        ArrayList<Square> squaresToControl = new ArrayList<>();
+        ArrayList<Square> squaresToControlNext = new ArrayList<>();
+        Square squareTemp;
+        Integer xTemp;
+        Integer yTemp;
+        int i = -1;
+        squaresToControl.add(square);
+        while(i < range){
+            while(!squaresToControl.isEmpty()){
+                squareTemp = squaresToControl.get(0);
+                xTemp = getCoord(squareTemp).get(0);
+                yTemp = getCoord(squareTemp).get(1);
+                if(squareTemp.getUp()!=Border.WALL){
+                    squaresToControlNext.add(getSquare(xTemp - 1, yTemp));
+                }
+                if(squareTemp.getDown()!=Border.WALL){
+                    squaresToControlNext.add(getSquare(xTemp + 1, yTemp));
+                }
+                if(squareTemp.getLeft()!=Border.WALL){
+                    squaresToControlNext.add(getSquare(xTemp, yTemp - 1));
+                }
+                if(squareTemp.getRight()!=Border.WALL){
+                    squaresToControlNext.add(getSquare(xTemp, yTemp + 1));
+                }
+                squaresToReturn.add(squareTemp);
+                squaresToControl.remove(0);
+            }
+            squaresToControlNext.removeAll(squaresToReturn);
+            squaresToControlNext =
+                    squaresToControlNext.stream().distinct()
+                            .collect(Collectors.toCollection(ArrayList::new));
+            squaresToControl.addAll(squaresToControlNext);
+            squaresToControlNext = new ArrayList<>();
+            i++;
+        }
+        return squaresToReturn;
+    }
+
+    /**
+     * Returns all the Squares that can be reached only and exactly with the number of moves passed
+     * by the parameter.
+     * <p>Attention! The Squares can be not visible from the starting square.</p>
+     * @param distance The exact number of moves that can be done.
+     * @param square The starting Square to calculate the distance.
+     * @return The list of Squares that can be reached only and exactly with the number of
+     * moves passed by the parameter.
+     */
+    public ArrayList<Square> getDistance(Square square, Integer distance){
+        ArrayList<Square> squaresToReturn = getRange(square, distance);
+        squaresToReturn.removeAll(getRange(square, distance - 1));
+        return squaresToReturn;
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/TestGameMapGetRange.java
@@ -0,0 +1,62 @@
+package it.polimi.ingsw;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests the normal functioning of the method getRange.
+ */
+class TestGameMapGetRange {
+    private GameMap gamemap;
+    private ArrayList<Square> expectedRange;
+    private ArrayList<Square> range;
+
+    @BeforeEach
+    void setUp() {
+        Square[][] grid;
+        grid = new Square[4][4];
+        expectedRange = new ArrayList<>();
+        grid[0][0] = new Square(Border.WALL, Border.WALL, Border.WALL, Border.NOTHING);
+        grid[0][1] = new Square(Border.WALL, Border.DOOR, Border.NOTHING, Border.NOTHING);
+        grid[0][2] = new Square(Border.WALL, Border.WALL, Border.NOTHING, Border.NOTHING);
+        grid[0][3] = new Square(Border.WALL, Border.NOTHING, Border.NOTHING, Border.WALL);
+        grid[1][0] = new Square(Border.NOTHING, Border.NOTHING, Border.WALL, Border.WALL);
+        grid[1][1] = new Square(Border.DOOR, Border.NOTHING, Border.WALL, Border.WALL);
+        grid[1][2] = new Square(Border.WALL, Border.NOTHING, Border.NOTHING, Border.WALL);
+        grid[1][3] = new Square(Border.NOTHING, Border.NOTHING, Border.WALL, Border.WALL);
+        grid[2][0] = new Square(Border.NOTHING, Border.WALL, Border.WALL, Border.WALL);
+        grid[2][1] = new Square(Border.NOTHING, Border.DOOR, Border.WALL, Border.NOTHING);
+        grid[2][2] = new Square(Border.NOTHING, Border.DOOR, Border.NOTHING, Border.WALL);
+        grid[2][3] = new Square(Border.NOTHING, Border.WALL, Border.WALL, Border.WALL);
+        grid[3][0] = new Square(Border.WALL, Border.WALL, Border.WALL, Border.NOTHING);
+        grid[3][1] = new Square(Border.DOOR, Border.WALL, Border.NOTHING, Border.WALL);
+        grid[3][2] = new Square(Border.DOOR, Border.WALL, Border.WALL, Border.NOTHING);
+        grid[3][3] = new Square(Border.WALL, Border.WALL, Border.NOTHING, Border.WALL);
+        gamemap = new GameMap(grid, new ArrayList<>(), new ArrayList<>());
+        expectedRange.add(grid[0][1]);
+        expectedRange.add(grid[1][1]);
+        expectedRange.add(grid[1][2]);
+        expectedRange.add(grid[2][1]);
+        expectedRange.add(grid[2][2]);
+        expectedRange.add(grid[3][0]);
+        expectedRange.add(grid[3][1]);
+        expectedRange.add(grid[3][2]);
+        expectedRange.add(grid[3][3]);
+        range = gamemap.getRange(grid[2][2], 3);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getRange() {
+        assertTrue(range.containsAll(expectedRange));
+        assertTrue(expectedRange.containsAll(range));
+    }
+}
