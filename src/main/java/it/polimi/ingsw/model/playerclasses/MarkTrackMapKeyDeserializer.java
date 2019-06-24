cc1ef3c10461d8d55669ccd4bdedfe93dfd758ca
Draghi96 <48287401+Draghi96@users.noreply.github.com>
2019-06-24 14:05:15 +0200

    Added Javadoc

--- src/main/java/it/polimi/ingsw/model/playerclasses/MarkTrackMapKeyDeserializer.java
+++ src/main/java/it/polimi/ingsw/model/playerclasses/MarkTrackMapKeyDeserializer.java
@@ -6,10 +6,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
+/**
+ * This class is used to deserialize a MarkTrack object from a JSON file.
+ *
+ * @author Draghi96
+ */
 public class MarkTrackMapKeyDeserializer extends KeyDeserializer {
 
+    /**
+     * This attribute is the Jackson object that builds and reads JSON files.
+     */
     private static final ObjectMapper mapper = new ObjectMapper();
-    
+
+    /**
+     * This method implements how to serialize a MarkTrack object into a JSON file.
+     *
+     * @param s a String object that is the serialized MarkTrack object.
+     * @param deserializationContext a DeserializationContext object containing all info about this deserialization file.
+     * @return a Object which is the deserialized MarkTrack object.
+     * @throws IOException if the file cannot be accessed correctly.
+     */
     @Override
     public Object deserializeKey(String s, DeserializationContext deserializationContext) throws IOException {
         return mapper.readValue(s,Player.class);
